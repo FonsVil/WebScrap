97bf9c2d155ee58529e783c3ce33a1b65a403768
Jazmine Espinoza <jaz11600@gmail.com>
2022-03-27 14:41:27 -0600

    prueba

---
+++ search_cars.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+url_page = 'http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx'
+
+page = requests.get(url_page).text 
+soup = BeautifulSoup(page, "lxml")
+	
+# Obtenemos la tabla por un ID específico
+tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblÍndices'})
+print(tabla)
